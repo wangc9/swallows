c814bcdfdc1ab7b400090a4ca71fb6f9dcc0a0e9
Chen Wang <wang.756090@gmail.com>
2024-03-23 18:05:17 +0200

    Feat: Add future section

--- app/visit-finland/page.tsx
+++ app/visit-finland/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Header from "../components/header";
 import Image from "next/image";
+import euroMap from '@/public/EuroMap.png';
 import location from'@/public/location.svg';
 
 export default function VisitFinland() {
@@ -11,13 +12,13 @@ export default function VisitFinland() {
   return (
     <div className="flex flex-col h-dvh content-center">
       <Header />
-      <main className="grid grid-cols-2 gap-x-2 h-full">
+      <main className="grid grid-cols-2 gap-x-2 gap-y-10 h-full">
         <div className="bg-[url('../public/Map.png')] bg-contain bg-no-repeat h-480 px-5 relative">
           {/* <Image className="top-5 left-5 absolute" src={location} width={50} height={50} alt="location 1" onClick={() => {setPlace('1');}} />
           <Image className="top-20 left-20 absolute" src={location} width={50} height={50} alt="location 2" onClick={() => {setPlace('2');}} />
           <Image className="top-32 left-32 absolute" src={location} width={50} height={50} alt="location 3" onClick={() => {setPlace('3');}} /> */}
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col self-center">
           <div className="flex w-full justify-evenly">
             <div className="rounded border-sky-400 border-2 flex flex-col w-24 grow">
               <p>Sites</p>
@@ -65,7 +66,10 @@ export default function VisitFinland() {
             </div>
           </div>
         </div>
-        {/* <Selection state={place} /> */}
+        <p className="text-xl self-center">Moving forward, the platform also holds the potential to help Visit Finland expand the eco travel initiative <b>beyond Finland</b>. By expanding to other European countries, Swallow can help Visit Finland by attracking broader European travellers into Finland, provide more income to local communities and attract more service providers to embark on the journey to transit towords sustainable operation.</p>
+        <Image className="justify-self-center" src={euroMap} height={480} alt="euro map" />
+        <div className="h-1" />
+        <div className="h-1" />
       </main>
     </div>
   )

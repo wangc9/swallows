1cdbe7ebf2ccf5dece113687f51584d6ed57885a
Chen Wang <wang.756090@gmail.com>
2024-03-23 11:54:27 +0200

    Feat: Change logo

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from "next/image"
-import placeholder from '../../public/placeholder.jpg';
+import logo from '@/public/SwallowsLogoTransparent.png';
 import { useRouter } from "next/navigation";
 
 export default function Header() {
@@ -9,7 +9,7 @@ export default function Header() {
 
   return (
     <header className="py-4">
-      <Image src={placeholder} width={100} height={100} alt="Group logo" onClick={() => {router.push('/')}} />
+      <Image src={logo} width={100} height={100} alt="Group logo" onClick={() => {router.push('/')}} />
     </header>
   )
 }
